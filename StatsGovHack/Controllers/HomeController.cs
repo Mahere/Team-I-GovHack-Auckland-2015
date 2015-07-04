c2bba646eab5980c12fcc7263c57e14dab3a532f
Andrew <test4@texrus.com>
2015-07-04 16:24:35 +1200

    added GetPopulationBySuburb web method

--- StatsGovHack/Controllers/HomeController.cs
+++ StatsGovHack/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace StatsGovHack.Controllers
 {
@@ -12,5 +15,37 @@ namespace StatsGovHack.Controllers
         {
             return View();
         }
+
+        
+        public JsonResult GetPopulationBySuburb(string suburb, string year)
+        {
+            if (HttpContext != null)
+            {
+                string fileName = Path.Combine(HttpContext.Request.PhysicalApplicationPath, "vw_popproj.json");
+
+                string n = VirtualPathUtility.ToAbsolute("~/vw_popproj.json");
+
+                using (var r = new StreamReader(fileName))
+                {
+                    string json = r.ReadToEnd();
+                    var items = JsonConvert.DeserializeObject<List<Item>>(json);
+                    var res = items.FirstOrDefault(i => (suburb.Equals(i.suburb, StringComparison.OrdinalIgnoreCase) &&
+                                                         year.Equals(i.year, StringComparison.OrdinalIgnoreCase)));
+                    if (res != null)
+                    {
+                        return Json(res.value, JsonRequestBehavior.AllowGet);
+                    }
+                }
+            }
+
+            return Json("0", JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class Item
+    {
+        public string year { get; set; }
+        public string suburb { get; set; }
+        public string value { get; set; }
     }
 }
