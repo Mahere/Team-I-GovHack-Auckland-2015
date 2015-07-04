3e2be53a13a79b1512ad53f6404e1f6dbdb09c89
nary-dev <nary_silver@hotmail.com>
2015-07-04 13:12:48 +1200

    saving geolocation

--- StatsGovHack/Scripts/Angular_Custom_App/googleMaps.js
+++ StatsGovHack/Scripts/Angular_Custom_App/googleMaps.js
@@ -4,6 +4,10 @@ var suburbs = {};
 suburbs['Auckland CBD'] = {
     center: new google.maps.LatLng(-36.846815, 174.766249)
 };
+suburbs['Takapuna'] = {
+    var location = getCoOrdinates('Takapuna');
+    center: new google.maps.LatLng(location.latitude,location.longitude)
+};
 
 var suburbCircle;
 
@@ -29,3 +33,12 @@ function initialize() {
     }
 }
 
+function getCoOrdinates(address) {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ 'address': address }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            return results[0].geometry.location.getLatLng();
+        } else
+            alert('error: ' + status);
+    });
+};
