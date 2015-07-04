5cb78753697d7019d74f3e1582c4063cb399aef7
shivendra odean <shivendra_odean@hotmail.com>
2015-07-04 19:29:23 +1200

    other graph

--- StatsGovHack/Scripts/Angular_Custom_App/graph.js
+++ StatsGovHack/Scripts/Angular_Custom_App/graph.js
@@ -35,18 +35,57 @@ var drawCrimeChart = function () {
 };
 
 var drawEducationChart = function (){
+    var data = crimeResult;
+    var convertedArr = [];
+    var tempArr = ['Year', 'House Price'];
+    convertedArr.push(tempArr);
+    for (var i = 0; i < data.length; i++) {
+        var currentElement = data[i];
+        var tempArr = [];
+        tempArr[0] = currentElement.year;
+        tempArr[1] = parseInt(currentElement.value);
+        convertedArr.push(tempArr);
+    }
+    console.log(convertedArr);
 
+    var graphData = google.visualization.arrayToDataTable(convertedArr);
+
+    var options = {
+        title: 'House Prices',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+    };
+    var chart = new google.visualization.LineChart(document.getElementById('education_chart'));
+    chart.draw(graphData, options);
 };
 
 var drawHousingChart = function () {
+    var data = crimeResult;
+    var convertedArr = [];
+    var tempArr = ['Year', 'Crime Rate'];
+    convertedArr.push(tempArr);
+    for (var i = 0; i < data.length; i++) {
+        var currentElement = data[i];
+        var tempArr = [];
+        tempArr[0] = currentElement.year;
+        tempArr[1] = parseInt(currentElement.value);
+        convertedArr.push(tempArr);
+    }
+    console.log(convertedArr);
 
+    var graphData = google.visualization.arrayToDataTable(convertedArr);
+
+    var options = {
+        title: 'Crime Rate',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+    };
+    var chart = new google.visualization.LineChart(document.getElementById('crime_chart'));
+    chart.draw(graphData, options);
 };
 
 
 //Retrieve Data
-
-
-
 var URL = "http://localhost:49313/";
 
 function getCrimeData(selectedSuburb) {
